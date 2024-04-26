07f1a923d103077746939d2ccd286ec2828ab511
Lalo64GG <223257@ids.upchiapas.edu.mx>
2024-04-25 19:13:09 -0600

    Add websocket server and update dependencies

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv'
+import http from 'http'
 import userRoute from './src/routes/user.route.js';
 import authRoute from './src/routes/auth.route.js';
 import bookRoute from './src/routes/library.route.js';
@@ -8,6 +9,7 @@ import cors from './src/middleware/cors.middleware.js';
 dotenv.config();
 
 const app = express();
+const server = http.createServer(app)
 
 app.use(cors)
 app.use(express.json())
@@ -16,6 +18,7 @@ app.use('/users', userRoute);
 app.use('/login', authRoute)
 app.use('/books', bookRoute);
 
+const websocketServer = 4000;
 const port = process.env.PORT || 5000;
 
 
